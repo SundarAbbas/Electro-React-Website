fc44ecf413fb15398944c4e1600588163bb879e1
SundarAbbas <chughtaiabbas486@gmail.com>
2026-02-28 13:48:36 +0500

    Added Client's Component

---
+++ src/Components/Clients.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Clients = () => {
+  return (
+    <div className="px-6 py-4 mt-10  flex flex-col  justify-center items-center">
+      <p className="text-gray-600 mb-5">Trusted By 45M Users</p>
+      
+      <div className="flex   justify-between lg:min-w-5xl   items-center flex-nowrap">
+        <img src="/user1.png" className="w-20 lg:w-28" alt="" />
+        <img src="/user2.png" className="w-20 lg:w-28" alt="" />
+        <img src="/user3.png" className="w-20 lg:w-28" alt="" />
+        <img src="/user4.png" className="w-20 lg:w-28" alt="" />
+        <img src="/user5.png" className="w-20 lg:w-28" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Clients;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Components/Hero";
+import Clients from "../Components/Clients";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Clients />
     </div>
   );
 };
